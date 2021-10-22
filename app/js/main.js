4047e817c656039c137ba8a0ce60c7a2dd2339fd
MCF512 <egdem4enko@yandex.ru>
2021-10-23 00:07:06 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function () {
-
+  $(".header__item").on("click", "a", function (event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 });
 
 $('.product__catalog').slick({
